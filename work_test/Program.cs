28dfdad4e6853a8e63cfc7f48c456d651499c8b0
Hmuriy Monakh <max_08931993@mail.ru>
2022-11-29 17:19:24 +0300

    new1

--- work_test/Program.cs
+++ work_test/Program.cs
@@ -1,5 +1,9 @@
 ﻿
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
 
+Console.WriteLine("Введите элемены массива:");
+string[] array = new string[size];
 
 
 
